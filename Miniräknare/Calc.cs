22c52dddcaa347d040e6ff136974ded7571ed11f
Marcus Medina <medina.jobb@outlook.com>
2021-12-03 13:21:17 +0100

    Add project files.

---
+++ Miniräknare/Calc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniräknare
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double SquareRoot(double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
---
+++ MiniräknareTests/CalculatorTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Miniräknare;
+
+namespace Miniräknare.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var calc = new Calculator();
+            var actual = calc.Add(5, 5);
+            var expected = 10;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            var calc = new Calculator();
+            var actual = calc.Subtract(5, 5);
+            var expected = 10;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            var calc = new Calculator();
+            var actual = calc.Multiply(5, 5);
+            var expected = 25;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            var calc = new Calculator();
+            var actual = calc.Divide(5, 5);
+            var expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PowerTest()
+        {
+            var calc = new Calculator();
+            var actual = calc.Power(5, 2);
+            var expected = 50;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SquareRootTest()
+        {
+            var calc = new Calculator();
+            var actual = calc.SquareRoot(5);
+            var expected = 2.23606797749979;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
